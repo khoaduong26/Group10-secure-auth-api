135890fc593dcfb74ac5469d0b4ce0e28bad8f65
danhdaden777 <minhdanh2824@gmail.com>
2026-05-12 22:49:02 +0700

    feat: add Reactjs UI; configuration files; add Postman result files

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const authRoutes =
-require("./routes/authRoutes");
-
-app.use("/api", authRoutes);
-app.use(express.json());
-
-module.exports = app;
